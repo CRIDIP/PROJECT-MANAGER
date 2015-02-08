368ba5308d310a31b63da1967062bb139b8d41c4
cridip <mmockelyn@cridip.com>
2015-02-08 18:45:44 +0100

    COMMIT 8215-4

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -14,7 +14,7 @@
                         <div class="sidebar-section sidebar-user clearfix">
                             <div class="sidebar-user-avatar">
                                 <a href="page_ready_user_profile.html">
-                                    <img src="img/placeholders/avatars/avatar2.jpg" alt="avatar">
+                                    <img src="<?php echo SITE,FOLDER,ASSETS; ?>img/placeholders/avatars/avatar2.jpg" alt="avatar">
                                 </a>
                             </div>
                             <div class="sidebar-user-name">John Doe</div>
@@ -27,39 +27,6 @@
                             </div>
                         </div>
                         <!-- END User Info -->
-
-                        <!-- Theme Colors -->
-                        <!-- Change Color Theme functionality can be found in js/app.js - templateOptions() -->
-                        <ul class="sidebar-section sidebar-themes clearfix">
-                            <li class="active">
-                                <a href="javascript:void(0)" class="themed-background-dark-default themed-border-default" data-theme="default" data-toggle="tooltip" title="Default Blue"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-night themed-border-night" data-theme="css/themes/night.css" data-toggle="tooltip" title="Night"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-amethyst themed-border-amethyst" data-theme="css/themes/amethyst.css" data-toggle="tooltip" title="Amethyst"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-modern themed-border-modern" data-theme="css/themes/modern.css" data-toggle="tooltip" title="Modern"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-autumn themed-border-autumn" data-theme="css/themes/autumn.css" data-toggle="tooltip" title="Autumn"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-flatie themed-border-flatie" data-theme="css/themes/flatie.css" data-toggle="tooltip" title="Flatie"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-spring themed-border-spring" data-theme="css/themes/spring.css" data-toggle="tooltip" title="Spring"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-fancy themed-border-fancy" data-theme="css/themes/fancy.css" data-toggle="tooltip" title="Fancy"></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" class="themed-background-dark-fire themed-border-fire" data-theme="css/themes/fire.css" data-toggle="tooltip" title="Fire"></a>
-                            </li>
-                        </ul>
-                        <!-- END Theme Colors -->
 
                         <!-- Sidebar Navigation -->
                         <ul class="sidebar-nav">
